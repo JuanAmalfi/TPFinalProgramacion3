7255eb026ec0188fdf2a0c90bd83375dcf0cf31a
Franco <francoleonel10@gmail.com>
2025-05-12 11:34:19 -0300

    Usuaruio Entity agregado

---
+++ src/main/java/com/example/demo/UsuarioEntity.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class UsuarioEntity {
+    private long idUsuario;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private int edad;
+}
